03dabdb13fe1d9482e7e341e45944d816ebe41d9
amitchivilkar <amitchivilkar@gmail.com>
2025-07-24 00:26:19 +0530

    Added favicon to metadata

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,12 @@ import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: "Social Sanad",
   description: "A website for to learn how technology works in Politics",
+  icons: {
+    icon: '/favicon.ico', // Path from public folder
+  },
 };
+
+
 
 export default function RootLayout({ children }) {
   return (
